5fe683f8aaf55cf825b4328d5ea0693fd5d34db8
OoTsun <gabriel.delhaye@hotmail.com>
2022-10-24 21:14:48 +0200

    Add logging customizations

--- src/service/core/winston/index.js
+++ src/service/core/winston/index.js
@@ -1,12 +1,17 @@
 import {format} from 'logform'
 import winston from 'winston'
 const { createLogger, transports } = winston
+
+const LOG_LEVEL = process.env.LOG_LEVEL || 'DEBUG'
+const LOG_DIR = process.env.LOG_DIR || 'log'
+const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : null;
+
 /**
  * Create a winston logger with console logging in dev, in prod everything is logged in an error file and combined file.
  * @type {winston.Logger}
  */
 const logger = createLogger({
-    level: 'debug',
+    level: LOG_LEVEL,
     format: format.combine(
         format.timestamp({
             format: 'YYYY-MM-DD HH:mm:ss'
@@ -16,7 +21,7 @@ const logger = createLogger({
         format.json()
     ),
     defaultMeta: {service: 'self-managed-discord'},
-    transports: process.env.NODE_ENV !== 'production' ? [
+    transports: NODE_ENV !== 'production' ? [
             new transports.Console({
                 format: format.combine(
                     format.colorize(),
@@ -32,8 +37,8 @@ const logger = createLogger({
                     format.simple()
                 )
             }),
-            new transports.File({ dirname: 'log', filename: 'self-managed-discord-error.log', level: 'error'}),
-            new transports.File({ dirname: 'log', filename: 'self-managed-discord-combined.log', level: 'info'})
+            new transports.File({ dirname: LOG_DIR, filename: 'self-managed-discord-error.log', level: 'ERROR'}),
+            new transports.File({ dirname: LOG_DIR, filename: 'self-managed-discord-combined.log', level: LOG_LEVEL})
         ]
 })
 export default logger
