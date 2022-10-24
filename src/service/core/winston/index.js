7f3c02262b58f46e1be472d1adc7575344872426
OoTsun <gabriel.delhaye@hotmail.com>
2022-10-24 22:12:21 +0200

    Add logging customizations

--- src/service/core/winston/index.js
+++ src/service/core/winston/index.js
@@ -1,44 +1,37 @@
 import {format} from 'logform'
 import winston from 'winston'
-const { createLogger, transports } = winston
 
-const LOG_LEVEL = process.env.LOG_LEVEL || 'DEBUG'
+const {createLogger, transports} = winston
+
+const LOG_LEVEL = process.env.LOG_LEVEL ? process.env.LOG_LEVEL.toLowerCase() : 'debug'
 const LOG_DIR = process.env.LOG_DIR || 'log'
 const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : null;
 
 /**
- * Create a winston logger with console logging in dev, in prod everything is logged in an error file and combined file.
+ * Create a winston logger with console logging in dev and console and file logging in prod
  * @type {winston.Logger}
  */
 const logger = createLogger({
-    level: LOG_LEVEL,
     format: format.combine(
         format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss'
+            format: 'HH:mm:ss:SSS',
         }),
         format.errors({stack: true}),
         format.splat(),
-        format.json()
+        format.json(),
+        format.colorize(),
+        format.printf(
+            (info) =>
+                `${info.timestamp} [${info.level}] ${info.message}`
+        )
     ),
-    defaultMeta: {service: 'self-managed-discord'},
-    transports: NODE_ENV !== 'production' ? [
-            new transports.Console({
-                format: format.combine(
-                    format.colorize(),
-                    format.simple()
-                )
-            })
-        ]
+    transports: NODE_ENV !== 'production' ?
+        [new transports.Console({level: LOG_LEVEL})]
         :
         [
-            new transports.Console({
-                format: format.combine(
-                    format.colorize(),
-                    format.simple()
-                )
-            }),
-            new transports.File({ dirname: LOG_DIR, filename: 'self-managed-discord-error.log', level: 'ERROR'}),
-            new transports.File({ dirname: LOG_DIR, filename: 'self-managed-discord-combined.log', level: LOG_LEVEL})
+            new transports.Console(),
+            new transports.File({dirname: LOG_DIR, filename: 'self-managed-discord-error.log', level: 'error'}),
+            new transports.File({dirname: LOG_DIR, filename: 'self-managed-discord-combined.log', level: LOG_LEVEL})
         ]
 })
 export default logger
